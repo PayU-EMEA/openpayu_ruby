1ada53abe229e900863cee7cdd642673ab6d14b3
Leszek Winiszewski <leszek.winiszewski@allegro.pl>
2013-09-19 11:36:35 +0200

    add refund model

--- lib/openpayu/models/refund.rb
+++ lib/openpayu/models/refund.rb
@@ -1,8 +1,8 @@
 module OpenPayU
   module Models
-    class Refund
-       attr_accessor :ext_refund_id, :type, :amount, :commission_amount, :proxy_commision_amount, :currency_code, :description, :bank_description, :source_account_number
-       validate :description, presence: true
+    class Refund < Model
+       attr_accessor :order_id, :ext_refund_id, :type, :amount, :commission_amount, :proxy_commision_amount, :currency_code, :description, :bank_description, :source_account_number
+       validates :description, :order_id, presence: true
     end
   end
 end
