68176186a6a73ee821ac6b67ff1f671662b19875
Leszek Winiszewski <leszek.winiszewski@allegro.pl>
2013-09-19 11:35:43 +0200

    add refund model spec

--- spec/unit/models/refund_spec.rb
+++ spec/unit/models/refund_spec.rb
@@ -1,5 +1,23 @@
 require "spec_helper.rb"
 
 describe OpenPayU::Models::Refund do
-  
+  context "create valid refund" do
+    let(:refund) do
+      OpenPayU::Models::Refund.new({
+        order_id: 1,
+        description: 'Refund',
+        amount: 1000
+      })
+    end
+    it { refund.valid?.should be_true }
+  end
+
+  context "create invalid refund" do
+    let(:refund) do
+      OpenPayU::Models::Refund.new({
+        amount: 1000
+      })
+    end
+    it { refund.valid?.should be_false }
+  end
 end
